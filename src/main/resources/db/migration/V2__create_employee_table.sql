b2a2bfde1507e328cf6da932afbc364db6bd690f
RaggedyRach <rachaelnolan@gmail.com>
2024-07-04 12:46:38 +0100

    cleaned up table

--- src/main/resources/db/migration/V2__create_employee_table.sql
+++ src/main/resources/db/migration/V2__create_employee_table.sql
@@ -3,13 +3,20 @@ create table Employees (
     employeeName varchar(70),
     employeeSalary decimal(7,2),
     employeeBankAccountNumber char(8),
-    employeeNationalInsuranceNumber char(9),
-
-
+    employeeNationalInsuranceNumber char(9)
 );
 
 INSERT into `Employees`
-(employeeName, employeeSalary,EmployeeBankAccountNumber, employeeNationalInsuranceNumber,
-employeeCommissionRate)
+(employeeName, employeeSalary,EmployeeBankAccountNumber, employeeNationalInsuranceNumber)
 values
-("Buddy",22000.00,12345678,'ab123456C');
+("Barb",28000.00,12345677,'FG123456C'),
+("Sarah",24000.00,12345699,'GH123456C'),
+("Mary",27000.00,12345699,'HI123456C'),
+("Max",22000.00,12345699,'JK123456C'),
+("John",44000.00,12345699,'LM123456C'),
+("Jack",34000.00,12345699,'MN123456C'),
+("Joe",38000.00,12345699,'NO123456C'),
+("Jen",32000.00,12345699,'PQ123456C'),
+("Jessica",28000.00,12345699,'RS123456C')
+;
+
