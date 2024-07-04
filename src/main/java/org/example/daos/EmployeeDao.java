a91c8a167cf278422ee5918b28aabe3d0ecdcf79
RaggedyRach <rachaelnolan@gmail.com>
2024-07-04 15:50:00 +0100

    removed any commented code

--- src/main/java/org/example/daos/EmployeeDao.java
+++ src/main/java/org/example/daos/EmployeeDao.java
@@ -1,48 +1,13 @@
 package org.example.daos;
 
-//import org.example.models.Employee;
 import org.example.models.EmployeeRequest;
-
-//import javax.ws.rs.core.Response;
 import java.sql.Connection;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.sql.ResultSet;
 import java.sql.PreparedStatement;
-//import java.util.ArrayList;
-//import java.util.List;
 
 public class EmployeeDao {
-
-//    public List<Employee> getAllEmployees() throws SQLException {
-//        List<Employee> employees = new ArrayList<>();
-//
-//        try (Connection connection = DatabaseConnector.getConnection()) {
-//            Statement statement = connection.createStatement();
-//            ResultSet resultSet = statement.executeQuery(
-//                    "select employeeID, employeeName ,employeeSalary, "
-//                            + "employeeBankAccountNumber,"
-//                            + "employeeNationalInsuranceNumber,"
-//                            + "employeeCommissionRate from Employees;");
-//
-//            while (resultSet.next()) {
-//                Employee order = new Employee(
-//                        resultSet.getInt(
-//                                "employeeID"),
-//                        resultSet.getString(
-//                                "employeeName"),
-//                        resultSet.getDouble(
-//                                "employeeSalary"),
-//                        resultSet.getString(
-//                                "employeeBankAccountNumber"),
-//                        resultSet.getString(
-//                                "employeeNationalInsuranceNumber"));
-//
-//                employees.add(order);
-//            }
-//        }
-//        return employees;
-//    }
 
     public int createEmployee(final EmployeeRequest employeeRequest)
             throws SQLException {
