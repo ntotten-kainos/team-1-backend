864d3e033f49dc9151c00a268a6adfdd6fdec886
dylanlaffin <dylan.laffin@kainos.com>
2024-07-04 15:25:40 +0100

    Delivery Employee files

---
+++ src/main/resources/db/migration/PopulateDelivery.sql
@@ -0,0 +1,12 @@
+INSERT INTO `DeliveryEmployee`(
+    Name,
+    BankAcctNum,
+    NINO,
+    Salary
+    )
+    VALUES (
+    "Joel barry",
+    "12345678",
+    "AB123456C",
+    "20000.00"
+    )
