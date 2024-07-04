5573455c8da5052fb076ae9e39aeafab45b47953
RaggedyRach <rachaelnolan@gmail.com>
2024-07-04 12:37:12 +0100

    added post request to employee controller

--- src/main/java/org/example/controllers/EmployeeController.java
+++ src/main/java/org/example/controllers/EmployeeController.java
@@ -1,8 +1,15 @@
 package org.example.controllers;
 
 import io.swagger.annotations.Api;
-
+import org.example.exceptions.FailedToCreateException;
+import org.example.models.EmployeeRequest;
+import org.example.services.EmployeeService;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import java.sql.SQLException;
 
 //"Engineering Academy - Group Challenge 2 - Team 3 - Sales Employee API")
 //@Path("/api/sales-employee"
@@ -11,5 +18,24 @@ import javax.ws.rs.Path;
 
 public class EmployeeController
 {
+     EmployeeService employeeService;
+
+    public EmployeeController(final EmployeeService employeeService) {
+        this.employeeService = employeeService;
+    }
+
+    @POST
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response createEmployee(EmployeeRequest employeeRequest)
+    {
+        try{
+            return Response
+                    .status(Response.Status.CREATED)
+                    .entity(employeeService.createEmployee(employeeRequest))
+                    .build();
+        } catch (SQLException | FailedToCreateException e) {
+            return Response.serverError().build();
+        }
+    }
 
 }
