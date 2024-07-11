a515853d9909513b8764e6ae6d02419535417a0d
ntotten-kainos <nathan.totten@kainos.com>
2024-07-11 10:11:18 +0100

    Needed a second client
    Name should also probably be unique?

--- src/main/resources/db/migration/V2__create_client_table.sql
+++ src/main/resources/db/migration/V2__create_client_table.sql
@@ -1,6 +1,6 @@
 CREATE TABLE `Client` (
     clientID int NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    clientName VARCHAR(64) NOT NULL
+    clientName VARCHAR(64) NOT NULL UNIQUE
 );
 
-INSERT INTO `Client`(`clientName`) VALUES ("Top Client");
+INSERT INTO `Client`(`clientName`) VALUES ("Top Client"), ("Other Client");
